7acaf8f764629d470349eac17b898dfaabff1918
aryan2001atat <aryan2001atat@gmail.com>
2024-12-28 11:58:32 +0530

    Updated hello.py for jenkins test

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-	return "This code has been changed to check the working of kubernetes!"
+	return "This code has been changed to check the working of kubernetes! and we are now working on Jenkins too."
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', debug = True)
