c7ec3607774fdc8800c30f2358533ecac31dcd5a
aryan2001atat <aryan2001atat@gmail.com>
2024-12-28 13:17:38 +0530

    update 3

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-	return "Update 2. This code has been changed to check the working of kubernetes! and we are now working on Jenkins too.\n Update 1.\n The Jenkins CICD pipeline is working!!!"
+	return "Update 3: The Jenkins CICD pipeline is working!!!"
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', debug = True)
